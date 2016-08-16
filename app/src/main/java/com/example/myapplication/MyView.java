b84288be831dada0f5501c0c0d28e6061fd5bac4
huangqiqiang <593979591@qq.com>
2016-08-16 21:27:45 +0800

    初步提交

--- app/src/main/java/com/example/myapplication/MyView.java
+++ app/src/main/java/com/example/myapplication/MyView.java
@@ -5,6 +5,7 @@ import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 import android.util.AttributeSet;
@@ -20,14 +21,22 @@ public class MyView extends View {
     private String mText;
     private int mDrawLeft;
     private int mDrawRight;
-    //
+
+    // 文字画笔
     private Paint mPaintText;
     private int mTextSize;
     private int mTextColor;
-    Rect mRect;
+    private Rect mRect; //  用于计算 text 的宽高
+    private float mTextPaddLeft;
 
+    // 左右 图片
     private Bitmap mRightBitmap;
     private Bitmap mleftBitmap;
+
+    private int mLineColor;
+    private  float mLineSize;
+    private  Paint mLinePaint;
+    private  int mLineStartMode;
 
     public MyView(Context context) {
         super(context);
@@ -41,10 +50,12 @@ public class MyView extends View {
         mDrawLeft = typeArray.getResourceId(R.styleable.MyView_drawLeft, R.mipmap.ic_launcher);
         mDrawRight = typeArray.getResourceId(R.styleable.MyView_drawRight, R.mipmap.ic_launcher);
 
-        // 设置字体
-        mPaintText = new Paint();
+
         mTextSize = (int) typeArray.getDimension(R.styleable.MyView_textSize, 16);
         mTextColor = typeArray.getColor(R.styleable.MyView_textColor, 0xFFFFFFFF);
+        mTextPaddLeft=typeArray.getDimensionPixelSize(R.styleable.MyView_textPaddLeft,0);
+        // 设置字体
+        mPaintText = new Paint();
         mPaintText.setColor(mTextColor);
         mPaintText.setTextSize(mTextSize);
         mPaintText.setAntiAlias(true);
@@ -54,6 +65,12 @@ public class MyView extends View {
 
         mRightBitmap = BitmapFactory.decodeResource(getResources(), mDrawRight);
         mleftBitmap = BitmapFactory.decodeResource(getResources(), mDrawLeft);
+
+        mLineColor=typeArray.getColor(R.styleable.MyView_lineColor,0xFFFFFFFF);
+        mLineSize=typeArray.getDimensionPixelSize(R.styleable.MyView_lineSize,10);
+        mLineStartMode=typeArray.getInt(R.styleable.MyView_lineStart,0);
+        mLinePaint=new Paint();
+        mLinePaint.setColor(mLineColor);
 
 
     }
@@ -68,18 +85,39 @@ public class MyView extends View {
 
 //        Log.e("111", "mRect : " + mRect.left + "  mRect : " + mRect.top);
 
-        //1 这个也可以湖中
+        //1 这个也可以剧中
 //        Paint.FontMetrics fontMetrics = mPaintText.getFontMetrics();
 //        float    offY = (fontMetrics.bottom - fontMetrics.top) / 2 - fontMetrics.bottom;
 //        canvas.drawText(mText, -mRect.left + getPaddingLeft() + mleftBitmap.getWidth(),getHeight()/2+offY, mPaintText);
         // 2 这个 比较居中
-      canvas.drawText(mText, -mRect.left + getPaddingLeft() + mleftBitmap.getWidth(),-mRect.top/2+ getHeight()/2, mPaintText);
+      canvas.drawText(mText, -mRect.left + getPaddingLeft() + mleftBitmap.getWidth() + mTextPaddLeft,-mRect.top/2+ getHeight()/2, mPaintText);
 
 //        Log(getPaddingLeft() + " getPaddingLeft() ");
 //        Log(getTop() + " getTop() ");
         canvas.drawBitmap(mRightBitmap, getWidth() - getPaddingRight() - mRightBitmap.getWidth(), getPaddingTop(), null);
         canvas.drawBitmap(mleftBitmap, getPaddingLeft(), getPaddingTop(), null);
+
+
+
+        drawLine(canvas);
     }
+
+    private void drawLine(Canvas canvas) {
+        switch (mLineStartMode){
+            case 0:
+                canvas.drawLine(getPaddingLeft(),getHeight()-mLineSize-getPaddingBottom(),getWidth()-getPaddingRight(),getHeight()-mLineSize-getPaddingBottom(),mLinePaint);
+                break;
+            case 2:
+
+                canvas.drawLine(getPaddingLeft()+mleftBitmap.getWidth()+mTextPaddLeft-mRect.left, getHeight()-mLineSize-getPaddingBottom(),
+                        getWidth()-getPaddingRight()- mRightBitmap.getWidth(),   getHeight()-mLineSize-getPaddingBottom(),mLinePaint
+                        );
+            break;
+        }
+
+
+    }
+
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
